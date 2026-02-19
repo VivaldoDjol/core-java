552da177e482806fcc65231b5b8bd30960d35a07
Gozzerks <VivaldoDjol@users.noreply.github.com>
2026-02-19 22:56:00 +0530

    Solve aDigitsSum.java

---
+++ CodeForces/aDigitsSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while (tests-- > 0) {
+            int n = sc.nextInt();
+            System.out.println((n + 1) / 10);
+        }
+        sc.close();
+    }
+}
