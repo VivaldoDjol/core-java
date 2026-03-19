677606f15f6b2fada76e4c94fee848bb064f38ba
Gozzerks <VivaldoDjol@users.noreply.github.com>
2026-03-19 21:14:49 +0000

    Solve bCopyCopyCopyCopyCopy.java

---
+++ CodeForces/bCopyCopyCopyCopyCopy.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tests = Integer.parseInt(br.readLine());
+
+        while (tests-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[n];
+            TreeSet <Integer> set = new TreeSet<>();
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+                set.add(arr[i]);
+            }
+            System.out.println(set.size());
+        }
+        br.close();
+    }
+}
