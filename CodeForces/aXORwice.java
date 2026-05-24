77175e1a74780bab55307bdc00bc4aad19c9df09
Gozzerks <VivaldoDjol@users.noreply.github.com>
2026-05-24 22:47:34 +0100

    Solve aXORwice.java

---
+++ CodeForces/aXORwice.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tests = Integer.parseInt(br.readLine());
+
+        while (tests-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println(a ^ b);
+        }
+    }
+}
